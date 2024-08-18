9e171a67a63132e89d7b5c3df209825c562f4b34
ltula <leandrotula@gmail.com>
2024-08-18 18:37:36 -0300

    Added page detail and added quantity to cart icon

--- src/components/purchase/PurchaseDetail.jsx
+++ src/components/purchase/PurchaseDetail.jsx
@@ -1,16 +1,42 @@
 import {useSelector} from "react-redux";
+import React from 'react';
 
 const PurchaseDetail = () => {
-
     const itemsCart = useSelector((state) => state.cart.items)
-    let allItems = new Set(itemsCart)
+    const totalFinal = itemsCart.reduce((acc, product) => acc + product.total_price, 0).toFixed(2);
 
     return (
-        <pre>
-            {JSON.stringify([...allItems], null, 3)}
-          </pre>
-
-    )
-}
+        <div className="container mt-4">
+            <h2 className="mb-4">Detalle de tu compra</h2>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Precio Unitario</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Total</th>
+                </tr>
+                </thead>
+                <tbody>
+                {itemsCart.map(product => (
+                    <tr key={product.id}>
+                        <td>
+                            <img src={product.image} alt={product.name} width="100" />
+                        </td>
+                        <td>{product.name}</td>
+                        <td>${product.price.toFixed(2)}</td>
+                        <td>{product.quantity}</td>
+                        <td>${product.total_price.toFixed(2)}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <div className="text-right mt-4">
+                <h4>Total a pagar: ${totalFinal}</h4>
+            </div>
+        </div>
+    );
+};
 
 export default PurchaseDetail;
