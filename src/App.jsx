c0edf6508f45027e5dc352beae9da651e8c2c1ab
ltula <leandrotula@gmail.com>
2024-08-18 19:16:58 -0300

    Added some cleanup to existing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/navbar/Navbar.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DefaultError from "./components/default/DefaultError.jsx";
-import Electrodomestic from "./components/items/electrodomestics/Electrodomestic.jsx";
 import Vehicles from "./components/items/vehicles/Vehicles.jsx";
 import CartDetail from "./components/items/vehicles/CardDetail.jsx";
 import PurchaseDetail from "./components/purchase/PurchaseDetail.jsx";
@@ -17,7 +16,6 @@ function App() {
                     <Route exact path="/autos" element={<Vehicles/>}/>
                     <Route exact path="/compra" element={<PurchaseDetail/>}/>
                     <Route path="/autos/:id" element={<CartDetail/>}/>
-                    <Route exact path="/electrodomesticos" element={<Electrodomestic/>}/>
                     <Route path="/*" element={<DefaultError/>}></Route>
                 </Routes>
             </ItemProvider>
--- src/components/items/electrodomestics/Electrodomestic.jsx
+++
@@ -1,9 +0,0 @@
-const Electrodomestic = () => {
-    return (
-        <div className="alert alert-danger" role="alert">
-            Seccion electrodomesticos aun no terminada
-        </div>
-    )
-}
-
-export default Electrodomestic;
